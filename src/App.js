24ce47132a6f6bb035bfa8801b7cc624e01f1605
MichiMar <michimar.alexis@gmail.com>
2019-11-06 15:41:32 -0700

    updated to Git Hub

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import request from "superagent";
+
+import "./App.css";
 
 function App() {
+  const [picture, setPicture] = React.useState("");
+  const [imgFromCloud, setImgFromCloud] = React.useState("");
+
+  const handleUpload = file => {
+    let upload = request
+      .post("https://api.cloudinary.com/v1_1/deivexiin/image/upload")
+      .field("upload_preset", "images")
+      .field("file", file);
+    upload.end((err, response) => {
+      if (err) {
+        console.log("cloudinary err", err);
+      }
+      if (response) {
+        setImgFromCloud(response.body.secure_url);
+      }
+    });
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="">
+      <input type="file" onChange={e => setPicture(e.target.files[0])} />
+      <button onClick={() => handleUpload(picture)}>Upload</button>
+      <img src={imgFromCloud} alt="picture" />
     </div>
   );
 }
